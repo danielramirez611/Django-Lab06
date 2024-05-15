7cfbc9c1c248be2705c27c6bfe89c18a5f105645
daniel <daniel.ramirez.s@tecsup.edu.pe>
2024-05-15 15:27:42 -0500

    Mensaje descriptivo de los cambios realizados

--- lab06/tienda/urls.py
+++ lab06/tienda/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('productos/', views.ProductoListView.as_view(), name='producto-list'),
     path('productos/<int:producto_id>/', views.ProductoDetailView.as_view(), name='producto-detail'),
+
+
 ]
